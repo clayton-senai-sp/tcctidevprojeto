b2761c0b41370d1478e384ceeb76a2f22861a9b1
Clayton Rodrigues <clayon.rodrigues@sp.senai.br>
2024-08-13 08:26:12 -0300

    Commit da estrutura inicial do projeto TCC

---
+++ src/main/java/br/senai/sp/tecdev1a1/Tecdev1a1Application.java
@@ -0,0 +1,13 @@
+package br.senai.sp.tecdev1a1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tecdev1a1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tecdev1a1Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/senai/sp/tecdev1a1/Tecdev1a1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.senai.sp.tecdev1a1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tecdev1a1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
